d318551cf05a73f45669c09913d1583d496f187d
Gabriel Pozato <gabriel.pozato@etec.sp.gov.br>
2023-06-01 21:34:30 -0300

    app cam

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FotoService } from '../services/foto.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(public fotoService: FotoService) {}
+
+  tirarFoto(){
+    this.fotoService.tirarFoto();
+  }
+
+}
