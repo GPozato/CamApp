253e61387ed037ed899af3f6897bd756be2709ae
Gabriel Pozato <gabriel.pozato@etec.sp.gov.br>
2023-06-20 20:35:39 -0300

    att projeto camera ai

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'camApp040A',
+  appName: 'camApp',
   webDir: 'www',
   server: {
     androidScheme: 'https'
